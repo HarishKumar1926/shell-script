fd712b78bda1c9cfd7d0ebd91db149d151955bb2
HarishKumar1926 <harishkumarurity@outlook.com>
2023-08-17 13:28:06 +0530

    Added Conversation between Ramesh and Suresh

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "This is my first shell scripting"
+echo "Ramesh : Hi Suresh ! Good Morning"
+echo "Suresh : Good Morning Ramesh"
+echo "Ramesh : How are you Suresh?"
+echo "Suresh : I am good Ramesh. How are you"
