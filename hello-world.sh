9da64f3648e66bb1f56b67a4341ad77803835bb5
HarishKumar1926 <harishkumarurity@outlook.com>
2023-08-17 13:34:53 +0530

    Added Conversation between Ramesh and Suresh using variables

--- hello-world.sh
+++ hello-world.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "This is my first shell scripting"
-echo "Ramesh : Hi Suresh ! Good Morning"
-echo "Suresh : Good Morning Ramesh"
-echo "Ramesh : How are you Suresh?"
-echo "Suresh : I am good Ramesh. How are you"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1 : Hi $PERSON2 ! Good Morning"
+echo "$PERSON2 : Good Morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2?"
+echo "$PERSON2 : I am good $PERSON1. How are you?"
