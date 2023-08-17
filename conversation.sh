728e31f89c1f9d5064d74e442b9b0082e073a669
HarishKumar1926 <harishkumarurity@outlook.com>
2023-08-17 13:43:42 +0530

    Conversation

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 : Hi $PERSON2 ! Good Morning"
+echo "$PERSON2 : Good Morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2?"
+echo "$PERSON2 : I am good $PERSON1. How are you?"
